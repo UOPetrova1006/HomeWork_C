5bd68e2d5a505cac84da1e5f81b86751259f8a2f
Uylia Petrova <cachorro1@ya.ru>
2022-08-10 22:06:44 +0300

    Ex2 added

---
+++ HomeWork_C_1/Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2. Ввести 2 числа, вывести максимум и минимум.
+
+while (true)
+{
+    Console.WriteLine("Введите s для продолжения или q для выхода");
+    string Operator = Console.ReadLine();
+     if (Operator == "q")
+     {
+        break;
+     }
+     else if (Operator == "s")
+     {
+        Console.WriteLine("Введите 1 число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите 2 число");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine($"max = {a}");
+        }
+        else if (a < b)
+        {
+            Console.WriteLine($"max = {b}");
+        }
+    }
+}
