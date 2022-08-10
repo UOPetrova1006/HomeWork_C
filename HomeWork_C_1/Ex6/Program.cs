63340cb612fc6c4a903dea5fbc613c16ba943334
Uylia Petrova <cachorro1@ya.ru>
2022-08-10 23:20:15 +0300

    Ex6 added

---
+++ HomeWork_C_1/Ex6/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+while (true)
+{
+    Console.WriteLine("Введите start для продолжения или quit для выхода");
+    string Operator = Console.ReadLine();
+     if (Operator == "quit")
+     {
+        break;
+     }
+     else if (Operator == "start")
+     {
+        Console.WriteLine("Введите число");
+        int a = Convert.ToInt32(Console.ReadLine());
+                
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("чётное");
+        }
+        else 
+        {
+        Console.WriteLine("нечётное");
+        }
+    }
+}
