93cd3f723d8c4dc4b2ea2d6fc8d90cd3ac6d364e
Uylia Petrova <cachorro1@ya.ru>
2022-08-10 23:49:11 +0300

    Ex8 added

---
+++ HomeWork_C_1/Ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа 
+// от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Все чётные числа от 1 до вашего числа:");
+int index = 1;
+
+while (index < N)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
