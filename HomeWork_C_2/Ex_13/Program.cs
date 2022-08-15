e2a324022b96746bfcbd6d5545f46e83e7012f77
Uylia Petrova <cachorro1@ya.ru>
2022-08-15 23:36:35 +0300

    HomeWork_C_2 added

---
+++ HomeWork_C_2/Ex_13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+using System;
+class Program
+{
+    public static void Main () 
+    {
+        Console.WriteLine("Введите число");
+        int num = Convert.ToInt32(Console.ReadLine());
+               
+        if (num < 100)
+        {
+            Console.WriteLine("Третьей цифры нет");    
+        }
+        else if (num > 99)
+        {
+            string numbers = num.ToString();
+            char[] chars = numbers.ToCharArray();
+            Console.WriteLine("Третья цифра заданного числа:" + chars[2]);           
+        }
+    }
+}
---
+++ HomeWork_C_2/Ex_15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+using System;
+class Program
+{
+    static void WriteSelect(string[] DaysArray)
+    {
+        for (int i = 0; i < DaysArray.Length-1; i++)
+        {
+            Console.Write(i+1 + " - " + DaysArray[i] + ", ");
+        }
+        Console.WriteLine(DaysArray.Length + " - " + DaysArray[DaysArray.Length-1] + ".");
+    }
+    static void SelectDay()
+    {
+        string [] NumberOfDay = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+        WriteSelect(NumberOfDay);
+        Console.WriteLine("Укажите номер дня недели");
+        int indexDays = Convert.ToInt32(Console.ReadLine());
+        if ((indexDays-1) < 5)
+        {
+                Console.WriteLine("будний день");     
+        }
+        else if ((indexDays-1) > 5)
+        {
+                Console.WriteLine("выходной");     
+        }
+    }
+    static void Main(string[] args)
+    {
+       SelectDay();   
+    }
+}
