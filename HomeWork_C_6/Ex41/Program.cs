6035002862c6ba1307e2c4e39a2e3d4e88e29984
Uylia Petrova <cachorro1@ya.ru>
2022-09-06 18:40:25 +0300

    HW_6

---
+++ HomeWork_C_6/Ex41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+class Program
+{
+    static void Main (string[] args)
+    {   
+        Console.WriteLine("Введите числа через запятую:");
+        string? Numbers = Console.ReadLine();
+        string[] array = Numbers.Split(",");
+        Console.WriteLine("Чисел больше нуля:");
+        Count(array);
+    }
+
+    static void Count (string [] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        if (Convert.ToInt32(array[i]) > 0)
+            {
+            count = count + 0; 
+            count++;  
+            }
+        Console.WriteLine(count);
+    }
+}
---
+++ HomeWork_C_6/Ex43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Прямые, заданны уравнениями: y = k1 * x + b1, y = k2 * x + b2. Для нахождения точки пересечения введите следующие данные...");
+            Console.WriteLine("k1");
+            double k1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b1");
+            double b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("k2");
+            double k2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b2");
+            double b2 = Convert.ToInt32(Console.ReadLine());
+            
+        //   k1 * x + b1 = k2 * x + b2  
+        //   k1 * X = k2 * X + b2 - b1
+        //   k1 * x - k2 * X = b2 - b1
+        //   (k1 - k2)*X = b2 - b1
+        //   x = (b2 -b1)/(k1 - k2)
+        double x = (b2 -b1)/(k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine("Точка пересечения прямых:");
+        Console.WriteLine($"({x}, {y})");
+        }
+    }
